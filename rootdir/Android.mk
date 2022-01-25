2f909d068bbd1611b6527899717a9bc44c57b2f4
Alfred Neumayer <dev.beidl@gmail.com>
2022-01-25 01:29:37 +0100

    rootdir: Ship dummy libnfc-nci.conf
    In order to allow libnfc-nci.conf to be bind-mounted over it
    needs the file to exist in the target inside of the container.
    Ship an empty libnfc-nci.conf file for that purpose.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,3 +7,11 @@ LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.waydroid.rc
 LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libnfc-nci.conf
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/libnfc-nci.conf
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)
+include $(BUILD_PREBUILT)
