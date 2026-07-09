91d83cf561627ee6cf36b4a3cb3bf94026ed4b64
TheKit <thekit@disroot.org>
2026-07-09 15:24:37 +0300

    manifests-36: use stock LineageOS + base-patches for the framework/system forks
    Stop forking frameworks/av, frameworks/base, frameworks/native, system/core
    and system/vold; use stock LineageOS (from default.xml) with the Waydroid
    changes applied from base-patches-36 by apply-patches.sh.
    
    Extracts the system/vold patches from the WayDroid-ATV fork vs LineageOS.
    packages/apps/TvSettings stays a fork for now.

--- manifest_scripts/generate-manifest.sh
+++ manifest_scripts/generate-manifest.sh
@@ -17,7 +17,7 @@
 top_dir=`pwd`
 LOCALDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 loc_man="${top_dir}/.repo/local_manifests"
-manifests_url="https://raw.githubusercontent.com/WayDroid-ATV/android_vendor_waydroid/lineage-23.2/manifest_scripts/manifests"
+manifests_url="https://raw.githubusercontent.com/waydroid/android_vendor_waydroid/dev/lineage-23.2/manifest_scripts/manifests"
 manifests_path="${LOCALDIR}/manifests"
 
 #setup colors
